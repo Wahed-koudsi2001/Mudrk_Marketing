02d2ad5f9a2c54f5ef13c2349fe997422881ab86
Wahed Koudsi <82679451+Wahed-koudsi2001@users.noreply.github.com>
2025-02-11 23:12:24 +0200

    uploads

--- JS/myJquery.js
+++ JS/myJquery.js
@@ -1,6 +1,3 @@
-/*jslint browser: true*/
-/*global $, jQuery, alert*/
-
 $(document).ready(function () {
   'use strict';
 
@@ -8,80 +5,11 @@ $(document).ready(function () {
     loop: true,
     spaceBetween: 20,
     speed: 2000,
-    // autoplay: {
-    //   delay: 7000,
-    //   disableOnInteraction: false,
-    // },
     navigation: {
       nextEl: '.s-next',
       prevEl: '.s-prev',
     }
   });
-
-  var swiper1 = new Swiper('.slider1', {
-    loop: true,
-    speed: 1000,
-    autoplay: {
-      delay: 5000,
-    },
-    spaceBetween: 16,
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: 'auto',
-    navigation: {
-      nextEl: '.s-next1',
-      prevEl: '.s-prev1',
-    }
-  });
-
-  var swiper2 = new Swiper('.slider2', {
-    loop: true,
-    speed: 1000,
-    autoplay: {
-      delay: 5000,
-    },
-    spaceBetween: 5,
-    navigation: {
-      nextEl: '.s-next2',
-      prevEl: '.s-prev2',
-    },
-    breakpoints: {
-      575: {
-        slidesPerView: 1,
-        spaceBetween: 0
-      },
-      768: {
-        slidesPerView: 2,
-        spaceBetween: 0
-      },
-      1200: {
-        slidesPerView: 3,
-        spaceBetween: 5
-      }
-    },
-  });
-
-
-  var swiper3 = new Swiper('.slider3', {
-    loop: true,
-    speed: 1000,
-    autoplay: {
-      delay: 5000,
-    },
-    spaceBetween: 20,
-    slidesPerView: 1,
-    navigation: {
-      nextEl: '.s-next3',
-      prevEl: '.s-prev3',
-    },
-    breakpoints: {
-      992: {
-        slidesPerView: 2,
-        spaceBetween: 20
-      }
-    },
-  });
-
 
   $("#back-top").click(function () {
     document.body.scrollTop = 0; // For Safari
